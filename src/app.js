91bdb6397a8a82503969190ce67a25f2d407f580
Mithras de Luca <mdlucata@gmail.com>
2022-09-15 23:58:02 -0300

    Mudanças de arquitetura

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 
 const indexRouter = require('./routes/indexRouter');
 const alunoRouter = require('./routes/alunoRouter');
@@ -12,15 +10,8 @@ const loginRouter = require('./routes/loginRouter');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 // routes engine setup
 app.use('/', indexRouter);
@@ -44,5 +35,10 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.listen(3006, () => {
+  console.log('teste');
+});
+
 
 module.exports = app;
